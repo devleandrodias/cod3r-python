3a138ae0ed951037572f7d92fe45706a4da205a0
devleandrodias <leandrodbdias@gmail.com>
2023-03-16 16:23:29 -0300

    feat: learning about logic operators

---
+++ 01_fundamentals/09_logic_operators.py
@@ -0,0 +1,43 @@
+# Logic Operators
+print(True or False)
+print(7 != 3 and 2 > 3)
+
+# Have money AND sunny (Truth Table (AND))
+print("I'll go to the mall?")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Have money OR sunny (Truth Table (OR))
+print("I'll go to the mall?")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# Have money XOR sunny (Truth Table (XOR))
+print("I'll go to the mall?")
+print(True != True)
+print(True != False)
+print(False != True)
+print(False != False)
+
+# Negation operator
+print('Negation operator')
+print(not True)
+print(not False)
+print(not 1)
+print(not 0)
+print(not not 1)
+
+# Warning!
+print('Bit a Bit')
+print(True & False)
+print(False | True)
+print(True ^ False)
+
+# 3 = 11
+# 2 = 10
+# _ = 10
+print(3 & 2)
