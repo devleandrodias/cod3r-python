b8824ccec714ac69366c954d563db31230647dbb
devleandrodias <leandrodbdias@gmail.com>
2023-03-16 19:51:50 -0300

    feat: learning about builtins

---
+++ 01_fundamentals/14_builtins.py
@@ -0,0 +1,9 @@
+# Builtins
+
+# __builtins__.print(__builtins__.type(1))
+# __builtins__.print(__builtins__.type('Hi'))
+# __builtins__.help(__builtins__.dir)
+# __builtins__.print(__builtins__.dir(__builtins__))
+
+name = 'Leandro'
+__builtins__.print(__builtins__.len(name))
