e7ad8aba9b6de2da259ad0b42d738fc703102167
devleandrodias <leandrodbdias@gmail.com>
2023-03-16 19:04:28 -0300

    feat: learning about unary operators

---
+++ 01_fundamentals/11_unary_operators.py
@@ -0,0 +1,22 @@
+# Unary operators
+
+# a++
+a = 1
+a += 1
+print(a)
+
+# b--
+b = 1
+b -= 1
+print(b)
+
+c = 1
+d = -1
+print(-c)
+print(+d)
+
+print(not 0)
+print(not 1)
+print(not -2)
+print(not False)
+print(not not False)
