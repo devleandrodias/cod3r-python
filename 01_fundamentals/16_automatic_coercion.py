2b0369f6f6dab94eb552bc95602384993d9f41a5
devleandrodias <leandrodbdias@gmail.com>
2023-03-18 15:21:15 -0300

    feat: learning about automatic coercion

---
+++ 01_fundamentals/16_automatic_coercion.py
@@ -0,0 +1,12 @@
+# Automatic Coercion
+
+print(10 / 2)  # Float
+print(type(10 / 2))
+
+print(10 // 3)  # Int
+print(type(10 // 3))  # Int
+print(type(10 // 3.3))  # Float
+
+print(2 + True)
+print(2 + False)
+print(2 + 1.5)
