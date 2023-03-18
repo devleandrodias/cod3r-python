9a06ff9e95018164c4cb8e22995a71328e8f7964
devleandrodias <leandrodbdias@gmail.com>
2023-03-18 15:47:51 -0300

    feat: finished v2 from challenge circumference area

---
+++ 02_first_program/circumference_area_v2.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+"""
+(Shebang)
+chmod 777 ./circumference_area_v2.py
+Circumference Area (π * R²)
+"""
+
+r = 15.3
+pi = 3.14159
+area = (pi * (r ** 2))
+
+print('A = π * {0}² = {1}'.format(r, area))
