8d0e0a1c489640cc133d897a96d732c1f22a8ff4
devleandrodias <leandrodbdias@gmail.com>
2023-03-18 15:16:33 -0300

    feat: learning about types conversion

---
+++ 01_fundamentals/15_types_conversion.py
@@ -0,0 +1,19 @@
+# Types Conversions
+
+a = 2
+b = '3'
+
+print(2 + 3)
+print('2' + '3')
+
+print(type(a))
+print(type(b))
+print(type(int(b)))
+
+print(a + int(b))
+print(str(a) + b)
+
+# print(a + int('2 cool'))
+
+print(a + float('2.2'))
+print(a + int('2.2'))
